cc892eafc561b8a8e0a5661f2e4a70fd03492dc6
khamsha <akhamatshin@gmail.com>
2023-11-25 01:00:14 +0300

    added tf and cloud-init for nfs and app hosts

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ terraform {
 provider "yandex" {
 }
 
+#################### ROUTER HOST ###################
+
 resource "yandex_compute_instance" "router" {
   name        = "router"
   platform_id = "standard-v1"
@@ -18,7 +20,7 @@ resource "yandex_compute_instance" "router" {
 
   resources {
     cores  = 2
-    memory = 4
+    memory = 2
   }
 
   boot_disk {
@@ -36,5 +38,66 @@ resource "yandex_compute_instance" "router" {
 
   metadata = {
     user-data = "${file("bootstrap/router.yaml")}"
+  }
+}
+
+#################### NFS HOST ######################
+
+resource "yandex_compute_instance" "nfs" {
+  name               = "nfs"
+  platform_id        = "standard-v1"
+  zone               = "ru-central1-b"
+  hostname           = "nfs"
+  service_account_id = "ajedf837co7ndvhrfbef"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd811ke2vnjc423tj8ji"
+    }
+  }
+
+  network_interface {
+    subnet_id      = "e2lrfl2ekcmg2j4mtqsu"
+    ip_address     = "10.129.0.20"
+    nat            = false
+  }
+
+  metadata = {
+    user-data = "${file("bootstrap/nfs.yaml")}"
+  }
+}
+
+#################### APP HOST ######################
+
+resource "yandex_compute_instance" "app" {
+  name               = "app-1"
+  platform_id        = "standard-v1"
+  zone               = "ru-central1-b"
+  hostname           = "app-1"
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd811ke2vnjc423tj8ji"
+    }
+  }
+
+  network_interface {
+    subnet_id      = "e2lrfl2ekcmg2j4mtqsu"
+    ip_address     = "10.129.0.30"
+    nat            = false
+  }
+
+  metadata = {
+    user-data = "${file("bootstrap/app.yaml")}"
   }
 }
